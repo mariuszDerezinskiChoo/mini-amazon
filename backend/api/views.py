57fc944eafa75129bc4b3bd59221bc0148a5aecf
Justin Du <jyd3@duke.edu>
2020-10-14 22:47:07 -0500

    testing edits

--- backend/api/views.py
+++ backend/api/views.py
@@ -82,46 +82,60 @@ def cart():
 ]
     return jsonify(result)
 
-@app.route('/recommended')
-def recommended():
-	technology = Item.query.filter_by(category='technology').all()
-	# technology = Item.query.filter_by(category='technology').order_by(Item.rating).all()
-	produce = Item.query.filter_by(category='produce').all()
-    sports = 
-    pets = 
-    render_template('recommended.html', category1=technology, category2=produce)
+# @main.route('/recommended')
+# def recommended():
+# 	technology = Item.query.filter_by(category='technology').all()
+# 	# technology = Item.query.filter_by(category='technology').order_by(Item.rating).all()
+# 	produce = Item.query.filter_by(category='produce').all()
+#     sports = 
+#     pets = 
+#     render_template('recommended.html', category1=technology, category2=produce)
 
-@app.route('/review/new', methods=['GET', 'POST'])
+# eventually, ideally clicking on an item on purchase history page should href to a url like
+# /loggedin-buyer_email/purchase-datetime/add_review (where buyer_email, datetime make a purchase primary key)
+@main.route('/add_review', methods=['GET', 'POST'])
 # @login_required
-def new_review():
-	# May need to import classes from *.models if in separate files
-	form = ReviewForm()
-	if form.validate_on_submit():
-		review = Reviews(
-			# id will populate automatically
-			buyer_email=form.buyer_email.data,
-			seller_email=form.seller_email.data,
-			# datetime_submitted is automatically populated - no need here, no need on form
-			item_id=form.item_id.data,
-			rating_item=form.rating_item.data,
-			rating_seller=form.rating_seller.data,
-			review=form.review.data
-			)
-		db.session.add(review)
-		db.session.commit()
-		flash('Your review has been submitted!', 'success')
-		return redirect(url_for('home'))
+def add_review():
+	# if form.validate_on_submit():
+    new_review = Reviews(
+        # id will populate automatically?
+        buyer_email=form.buyer_email.data,
+        seller_email=form.seller_email.data,
+        # datetime_submitted is automatically populated?
+        item_id=form.item_id.data,
+        rating_item=form.rating_item.data,
+        rating_seller=form.rating_seller.data,
+        review=form.review.data
+        )
+    db.session.add(new_review)
+    db.session.commit()
+    # flash('Your review has been submitted!', 'success')
 	return render_template('create_review.html', title='Write Review', form=form, legend='Write Review')
 
+@main.route('/add_user', methods=['POST'])
+def add_user():
+    user_data = request.get_json()
+
+    new_user = User(email = user_data['email'],
+                    password = user_data['password'], 
+                    first_name = user_data['first_name'],
+                    last_name = user_data['last_name'],
+                    balance = user_data['balance'], 
+                    )
+    db.session.add(new_user)
+    db.session.commit()
+
+    return 'Done', 201
+
 # review_id is Reviews(id)
-@app.route('/review/<int:review_id>', methods=['GET', 'POST'])
+@main.route('/reviews/<int:review_id>', methods=['GET', 'POST'])
 def review(review_id):
 	review = Reviews.query.get_or_404(review_id)
 	return render_template('review.html', title='See Review', review=review)
 	# TODO: Create HTML template for looking at review, offer button that will take user
 	# to the update page V
 
-@app.route('/review/<int:review_id>/update', methods=['GET', 'POST'])
+@main.route('/review/<int:review_id>/update', methods=['GET', 'POST'])
 # @login_required
 def update_review(review_id):
 	review = Reviews.query.get_or_404(review_id)
