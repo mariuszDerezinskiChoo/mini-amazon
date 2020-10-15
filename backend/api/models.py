aee3b904c5188a82cf5ab7bc90c9474ced07b246
Karoline Xiong <karoline.xiong@duke.edu>
2020-10-14 22:57:17 -0400

    having problems

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,24 +1,31 @@
 from . import db 
 from datetime import datetime
 
-class User(db.Model):
-    __tablename__ = 'user'
+class Buyer(db.Model):
+    __tablename__ = 'buyer'
     __table_args__ = {'extend_existing': True}
     email = db.Column(db.String(30), primary_key=True)
     password = db.Column(db.String(30),nullable=False)
     first_name = db.Column(db.String(20),nullable=False)
     last_name = db.Column(db.String(20),nullable=False)
     balance = db.Column(db.Integer, nullable=False)
-    is_seller = db.Column(db.Boolean, default=False)
-    seller_desc = db.Column(db.Text, nullable=False)
 
+class Storefront(db.Model):
+    __tablename__ = 'storefront'
+    email = db.Column(db.String(30),primary_key=True)
+    password = db.Column(db.String(30),nullable=False)
+    name = db.Column(db.String(30),nullable=False)
+    balance = db.Column(db.Integer, nullable=False)
+    photo_url = db.Column(db.String(100),nullable=False)
+    description = db.Column(db.Text,nullable=False)
 
 class Item(db.Model):
 	# This is automatically populated for every valid entry made into the db
-	id = db.Column(db.Integer, primary_key=True)
-	category = db.Column(db.String(30), nullable=False)
-	name = db.Column(db.String(30), nullable=False)
-	description = db.Column(db.Text, nullable=False)
+    __tablename__ = 'item'
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(30), nullable=False)
+    name = db.Column(db.String(30), nullable=False)
+    description = db.Column(db.Text, nullable=False)
 	# image_url
 
 class Reviews(db.Model):
@@ -28,34 +35,33 @@ class Reviews(db.Model):
 	# buyer_email = db.Column(db.String(30), db.ForeignKey('user.email'), primary_key=True)
 	# seller_email = db.Column(db.String(30), db.ForeignKey('user.email'), primary_key=True)
 	# datetime_submitted = db.Column(db.DateTime, default=datetime.utcnow, primary_key=True)
-	id = db.Column(db.Integer, nullable=False, primary_key=True)
-	buyer_email = db.Column(db.String(30), db.ForeignKey('user.email'))
-	seller_email = db.Column(db.String(30), db.ForeignKey('user.email'))
-	datetime_submitted = db.Column(db.DateTime, default=datetime.utcnow)
+	buyer_email = db.Column(db.String(30), db.ForeignKey('buyer.email'), primary_key=True)
+	seller_email = db.Column(db.String(30), db.ForeignKey('storefront.email'))
+	datetime_submitted = db.Column(db.DateTime, default=datetime.utcnow, primary_key=True)
 
 	item_id = db.Column(db.Integer, db.ForeignKey('item.id'),nullable=False)
 	rating_item = db.Column(db.Integer, nullable=False)
 	rating_seller = db.Column(db.Integer)
 	review = db.Column(db.Text, nullable=False)
 
-class Selling(db.Model):
-    seller_email = db.Column(db.String(30), db.ForeignKey('user.email'),primary_key=True)
-    item_id = db.Column(db.Integer, primary_key=True)
+class Listing(db.Model):
+    seller_email = db.Column(db.String(30), db.ForeignKey('storefront.email'),primary_key=True)
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'),primary_key=True)
     price = db.Column(db.Integer,nullable=False)
     quantity = db.Column(db.Integer,nullable=False)
 
 class Purchase(db.Model):
-    seller_email = db.Column(db.String(30), db.ForeignKey('user.email'),primary_key=True)
+    seller_email = db.Column(db.String(30), db.ForeignKey('storefront.email'),primary_key=True)
     item_id = db.Column(db.Integer, db.ForeignKey('item.id'), primary_key=True)
-    buyer_email = db.Column(db.String(30), db.ForeignKey('user.email'),primary_key=True)
+    buyer_email = db.Column(db.String(30), db.ForeignKey('buyer.email'),primary_key=True)
     price = db.Column(db.Integer,nullable=False)
     quantity = db.Column(db.Integer,nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow,nullable=False)
 
 class Cart(db.Model):
-    seller_email = db.Column(db.String(30), db.ForeignKey('user.email'),primary_key=True)
+    seller_email = db.Column(db.String(30), db.ForeignKey('storefront.email'),primary_key=True)
     item_id = db.Column(db.Integer, db.ForeignKey('item.id'), primary_key=True)
-    buyer_email = db.Column(db.String(30), db.ForeignKey('user.email'),primary_key=True)
+    buyer_email = db.Column(db.String(30), db.ForeignKey('buyer.email'),primary_key=True)
     quantity = db.Column(db.Integer,nullable=False)
 
 
